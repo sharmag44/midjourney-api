f28482e4d26236bf544477367546e1209037f254
shambhu_bcoder <shambhunath@bcoder.in>
2023-03-30 18:16:21 +0530

    code updated

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const result = async () => {
             "yuval-alaluf/sam:9222a21c181b707209ef12b5e0d7e94c994b58f01c7b2fec075d2e892362f13c",
             {
                 input: {
-                    image: "https://replicate.delivery/mgxm/806bea64-bb51-4c8a-bf4d-15602eb60fdd/1287.jpg",
-                    target_age: "100"
+                    image: "https://bcoder-dev-bucket.s3.amazonaws.com/couchvibes/dev/ed2112a187949a720aecac653d0dc7cf77171e39.jfif",
+                    target_age: "40"
                 }
             }
         );
